ac940bba3455af17c2b280f21e7869d11c3f8195
Brandon Chinn <brandon@leapyear.io>
2019-03-14 14:55:44 -0700

    Add script to query GitHub graphql api

---
+++ gh_graphql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -u -o pipefail
+
+SCRIPT=
+VARS='{}'
+TOKEN=
+
+usage() {
+    echo "Usage: ${0} [--vars VARS] [--token TOKEN] FILE"
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        (--vars) shift; VARS="$1" ;;
+        (--token) shift; TOKEN="$1" ;;
+        (--help) usage; exit 0 ;;
+        (*)
+            if [[ -z "${SCRIPT}" ]]; then
+                SCRIPT="$(cat "$1" | xargs echo)"
+            else
+                usage; exit 1
+            fi
+        ;;
+    esac
+    shift
+done
+
+if [[ -z "${SCRIPT}" ]]; then
+    usage; exit 1
+fi
+
+CURL_ARGS=(
+    -H "Authorization: Bearer ${TOKEN}"
+    -H "Accept: application/vnd.github.antiope-preview+json"
+    --data "{\"query\": \"${SCRIPT}\", \"variables\": ${VARS}}"
+)
+
+curl "${CURL_ARGS[@]}" https://api.github.com/graphql
