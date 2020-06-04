35d18126d9fbfc11eb44912e6f384198318797f3
Brandon Chinn <brandon@leapyear.io>
2020-06-04 10:36:53 -0700

    Add graphql introspection query

---
+++ graphql_introspection_query.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+#
+# Usage: curl $GQL_ENDPOINT -d "$(./graphql_introspection_query.py)"
+
+import json
+
+# https://github.com/graphql/graphql-js/pull/2124/files#diff-4381e15d00ce4b749116b59bcbee9863
+INTROSPECTION_QUERY="""
+query IntrospectionQuery {
+  __schema {
+    queryType { name }
+    mutationType { name }
+    subscriptionType { name }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+
+fragment InputValue on __InputValue {
+  name
+  description
+  type { ...TypeRef }
+  defaultValue
+}
+
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+print(json.dumps({ 'query': INTROSPECTION_QUERY }))
